1729110c690b6fd9a8268d4a0e18f9c12d340660
“Moon <111060012+moiseskf@users.noreply.github.com>
2024-02-14 19:20:26 -0300

    Commit #2: Botao salvar

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -2,41 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-/**
- *
- * @author Adm
- */
-
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
-    Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    private conectaDAO conexao;
+    private Connection conn;
+
+    public ProdutosDAO() {
+        this.conexao = new conectaDAO();
+        this.conn = this.conexao.connectDB(); // objeto "conn" esta recebendo a conexao retornada por conexao.getConexao()
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    PreparedStatement prep; //manipulacao de dados
+    ResultSet resultset; // consultas
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        int status;
+
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos(nome,valor) VALUES(?,?)");
+
+            prep.setString(1, produto.getNome());
+            prep.setString(2, Integer.toString(produto.getValor()));
+
+            status = prep.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+
+        }
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -4,30 +4,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
+
     
-    public Connection connectDB(){
+
+    public Connection connectDB() {
         Connection conn = null;
-        
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/exemplo_senac?useSSL=false", "root", "E9I)ejKPSfeo=");
+            return conn;
+
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+            return conn;
+
         }
-        return conn;
+
     }
-    
+
 }
